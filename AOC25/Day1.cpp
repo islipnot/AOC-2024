19ac56e62bc20a88ced80c1fa8d0ff1222e289af
vyxblvs <newdiscord69420@proton.me>
2025-01-13 01:15:40 -0800

    Add project files.

---
+++ AOC25/Day1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int Part1()
+{
+	std::ifstream file("d:\\input.txt");
+	std::string line;
+
+	int TotalDist = 0;
+	std::vector<int> LeftRow, RightRow;
+
+	while (std::getline(file, line))
+	{
+		static const int RowOffset = line.find_last_of(' ') + 1;
+
+		LeftRow.emplace_back(std::stoi(line));
+		RightRow.emplace_back(std::stoi(line.substr(RowOffset)));
+	}
+
+	std::sort(LeftRow.begin(), LeftRow.end());
+	std::sort(RightRow.begin(), RightRow.end());
+
+	for (int i = 0, s = LeftRow.size(); i < s; ++i)
+	{
+		TotalDist += std::abs(LeftRow[i] - RightRow[i]);
+	}
+
+	std::cout << "Total Distance: " << TotalDist << '\n';
+
+	return 0;
+}
+
+int Part2()
+{
+	std::ifstream file("d:\\input.txt");
+	std::string line;
+
+	uint32_t score = 0;
+	std::vector<int> LeftRow, RightRow;
+
+	while (std::getline(file, line))
+	{
+		static const int RowOffset = line.find_last_of(' ') + 1;
+
+		LeftRow.emplace_back(std::stoi(line));
+		RightRow.emplace_back(std::stoi(line.substr(RowOffset)));
+	}
+
+	for (int num : LeftRow)
+	{
+		score += num * std::count(RightRow.begin(), RightRow.end(), num);
+	}
+
+	std::cout << "Similarity Score: " << score << '\n';
+
+	return 0;
+}
---
+++ AOC25/Day2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::ifstream file("D:\\input.txt");
+	std::string line;
+
+	while (std::getline(file, line))
+	{
+
+	}
+
+	return 0;
+}
