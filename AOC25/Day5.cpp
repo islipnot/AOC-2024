5d99d8c6a4d6cac88d7965ef12c05caf1c4e3f49
vyxblvs <newdiscord69420@proton.me>
2025-04-05 20:43:06 -0700

    day 5

--- AOC25/Day5.cpp
+++ AOC25/Day5.cpp
@@ -1,8 +1,20 @@
 #include "pch.h"
 #include <vector>
+#include <sstream>
 #include <algorithm>
+#include <unordered_set>
 
-bool IsValidUpdate(std::vector<std::string>& rules, std::string update)
+std::unordered_set<std::string> FillRuleSet(std::ifstream& file)
+{
+	std::vector<std::string> rules;
+	do { rules.push_back({}); } while (std::getline(file, rules.back()) && !rules.back().empty());
+
+	rules.pop_back();
+
+	return std::unordered_set<std::string>(rules.begin(), rules.end());
+}
+
+bool IsValidUpdate(std::unordered_set<std::string>& rules, std::string update)
 {
 	while (update.size() > 2)
 	{
@@ -10,19 +22,10 @@ bool IsValidUpdate(std::vector<std::string>& rules, std::string update)
 
 		while (TempUpdate.size() > 3)
 		{
-			bool FoundRule = false;
-			const std::string NeededRule = TempUpdate.substr(0, 5);
-
-			for (std::string& rule : rules)
+			if (rules.find(TempUpdate.substr(0, 5)) == rules.end())
 			{
-				if (rule == NeededRule)
-				{
-					FoundRule = true;
-					break;
-				}
+				return false;
 			}
-
-			if (!FoundRule) return false;
 			
 			TempUpdate.erase(3, 3);
 		}
@@ -33,28 +36,98 @@ bool IsValidUpdate(std::vector<std::string>& rules, std::string update)
 	return true;
 }
 
-int main()
+int d5p1()
 {
 	std::ifstream file("d:\\input.txt");
-	
-	std::vector<std::string> rules;
-	do { rules.push_back({}); } while (std::getline(file, rules.back()) && !rules.back().empty());
+	std::unordered_set<std::string> rules = FillRuleSet(file);
 	
 	std::vector<std::string> updates;
 	do { updates.push_back({}); } while (std::getline(file, updates.back()));
 
 	file.close();
-	rules.pop_back();
 	updates.pop_back();
 
 	int result = 0;
 
-	for (std::string update : updates)
+	for (std::string& update : updates)
 	{
 		std::replace(update.begin(), update.end(), ',', '|');
 
 		if (IsValidUpdate(rules, update))
 		{
+			const int MidOffset = std::count(update.begin(), update.end(), '|') * 1.5f;
+			result += std::stoi(update.substr(MidOffset));
+		}
+	}
+
+	std::cout << "Result: " << result << '\n';
+
+	return 0;
+}
+
+void FixUpdate(std::unordered_set<std::string>& rules, std::string& update)
+{
+	std::stringstream ss(update);
+	std::vector<int> nums;
+
+	for (std::string StrNum; std::getline(ss, StrNum, '|'); nums.emplace_back(std::stoi(StrNum)));
+
+	const int sz = static_cast<int>(nums.size());
+	std::vector<int> pos(sz, 0);
+
+	for (int i = 0; i < sz; ++i)
+	{
+		const int TargNum = nums[i];
+
+		for (int num : nums)
+		{
+			if (num == TargNum) continue;
+
+			const std::string query = std::to_string(TargNum) + '|' + std::to_string(num);
+
+			if (rules.find(query) == rules.end()) ++pos[i];
+		}
+	}
+
+	std::vector<int> NewUpdate(sz);
+
+	for (int i = 0; i < sz; ++i)
+	{
+		NewUpdate[pos[i]] = nums[i];
+	}
+
+	std::ostringstream oss;
+
+	for (int i = 0; i < sz; ++i)
+	{
+		oss << NewUpdate[i];
+		if (i < sz - 1) oss << '|';
+	}
+
+	update = oss.str();
+}
+
+int main()
+{
+	std::ifstream file("d:\\input.txt");
+	std::unordered_set<std::string> rules = FillRuleSet(file);
+
+	std::vector<std::string> updates;
+	do { updates.push_back({}); } while (std::getline(file, updates.back()));
+
+	file.close();
+	updates.pop_back();
+
+	int result = 0;
+
+	for (std::string& update : updates)
+	{
+		std::replace(update.begin(), update.end(), ',', '|');
+
+		if (!IsValidUpdate(rules, update))
+		{
+			FixUpdate(rules, update); 
+
 			const int MidOffset = std::count(update.begin(), update.end(), '|') * 1.5f;
 			result += std::stoi(update.substr(MidOffset));
 		}
